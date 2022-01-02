bce94d56067eee62c1febbc7f44e0a4cb224c44c
nullium21 <rommyskovets@gmail.com>
2022-01-02 19:24:39 +0200

    Add a Installate manifest for uilib

---
+++ installate/registry/uilib.pkg.lua
@@ -0,0 +1,11 @@
+return {
+  name = "Null's UI Library",
+
+  files = {
+    ["/lib/uilib/color.lua"]   = { source = "github", repo = "nullium21/oc-prog", path = "gui-eeprom/uilib/color.lua" },
+    ["/lib/uilib/dbuffer.lua"] = { source = "github", repo = "nullium21/oc-prog", path = "gui-eeprom/uilib/dbuffer.lua" },
+    ["/lib/uilib/init.lua"]    = { source = "github", repo = "nullium21/oc-prog", path = "gui-eeprom/uilib/init.lua" },
+    ["/lib/uilib/object.lua"]  = { source = "github", repo = "nullium21/oc-prog", path = "gui-eeprom/uilib/object.lua" },
+    ["/lib/uilib/util.lua"]    = { source = "github", repo = "nullium21/oc-prog", path = "gui-eeprom/uilib/util.lua" }
+  }
+}
