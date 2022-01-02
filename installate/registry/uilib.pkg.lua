aac259b398734f5b446493c6e88d78fc8de6d567
nullium21 <rommyskovets@gmail.com>
2022-01-02 19:30:54 +0200

    Fix branch in uilib.pkg.lua

--- installate/registry/uilib.pkg.lua
+++ installate/registry/uilib.pkg.lua
@@ -2,10 +2,10 @@ return {
   name = "Null's UI Library",
 
   files = {
-    ["/lib/uilib/color.lua"]   = { source = "github", repo = "nullium21/oc-prog", path = "uilib/color.lua" },
-    ["/lib/uilib/dbuffer.lua"] = { source = "github", repo = "nullium21/oc-prog", path = "uilib/dbuffer.lua" },
-    ["/lib/uilib/init.lua"]    = { source = "github", repo = "nullium21/oc-prog", path = "uilib/init.lua" },
-    ["/lib/uilib/object.lua"]  = { source = "github", repo = "nullium21/oc-prog", path = "uilib/object.lua" },
-    ["/lib/uilib/util.lua"]    = { source = "github", repo = "nullium21/oc-prog", path = "uilib/util.lua" }
+    ["/lib/uilib/color.lua"]   = { source = "github", repo = "nullium21/oc-prog", branch = "uilib", path = "uilib/color.lua" },
+    ["/lib/uilib/dbuffer.lua"] = { source = "github", repo = "nullium21/oc-prog", branch = "uilib", path = "uilib/dbuffer.lua" },
+    ["/lib/uilib/init.lua"]    = { source = "github", repo = "nullium21/oc-prog", branch = "uilib", path = "uilib/init.lua" },
+    ["/lib/uilib/object.lua"]  = { source = "github", repo = "nullium21/oc-prog", branch = "uilib", path = "uilib/object.lua" },
+    ["/lib/uilib/util.lua"]    = { source = "github", repo = "nullium21/oc-prog", branch = "uilib", path = "uilib/util.lua" }
   }
 }
