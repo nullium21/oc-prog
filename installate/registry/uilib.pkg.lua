ef7eb72edfded8a4e323677f71b0272c20daf0d4
nullium21 <rommyskovets@gmail.com>
2022-01-02 19:25:55 +0200

    Fix file paths in uilib.pkg.lua

--- installate/registry/uilib.pkg.lua
+++ installate/registry/uilib.pkg.lua
@@ -2,10 +2,10 @@ return {
   name = "Null's UI Library",
 
   files = {
-    ["/lib/uilib/color.lua"]   = { source = "github", repo = "nullium21/oc-prog", path = "gui-eeprom/uilib/color.lua" },
-    ["/lib/uilib/dbuffer.lua"] = { source = "github", repo = "nullium21/oc-prog", path = "gui-eeprom/uilib/dbuffer.lua" },
-    ["/lib/uilib/init.lua"]    = { source = "github", repo = "nullium21/oc-prog", path = "gui-eeprom/uilib/init.lua" },
-    ["/lib/uilib/object.lua"]  = { source = "github", repo = "nullium21/oc-prog", path = "gui-eeprom/uilib/object.lua" },
-    ["/lib/uilib/util.lua"]    = { source = "github", repo = "nullium21/oc-prog", path = "gui-eeprom/uilib/util.lua" }
+    ["/lib/uilib/color.lua"]   = { source = "github", repo = "nullium21/oc-prog", path = "uilib/color.lua" },
+    ["/lib/uilib/dbuffer.lua"] = { source = "github", repo = "nullium21/oc-prog", path = "uilib/dbuffer.lua" },
+    ["/lib/uilib/init.lua"]    = { source = "github", repo = "nullium21/oc-prog", path = "uilib/init.lua" },
+    ["/lib/uilib/object.lua"]  = { source = "github", repo = "nullium21/oc-prog", path = "uilib/object.lua" },
+    ["/lib/uilib/util.lua"]    = { source = "github", repo = "nullium21/oc-prog", path = "uilib/util.lua" }
   }
 }
