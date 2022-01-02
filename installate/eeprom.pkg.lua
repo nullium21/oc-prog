cd085a559e807df74cd543901f3f53594e767f5e
nullium21 <rommyskovets@gmail.com>
2022-01-02 15:04:13 +0200

    Add a manifest for the bootloader

---
+++ installate/eeprom.pkg.lua
@@ -0,0 +1,18 @@
+return {
+  name = "GUI Bootloader",
+
+  files = {
+    ["/eeprom.min.lua"] = { source = "github", repo = "nullium21/oc-prog", path = "gui-eeprom/eeprom.min.lua" }
+  },
+
+  postinstall = function ()
+    local eeprom = require("component").eeprom
+
+    local file = io.open("/eeprom.min.lua", "r")
+    local data = file:read("*a")
+    file:close()
+
+    eeprom.set(data)
+    eeprom.setLabel("GUI Bootloader")
+  end
+}
